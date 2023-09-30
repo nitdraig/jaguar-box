76b0ba3dbb266cfbe9ccc0692fb05bc112277d1f
Agustín Avellaneda <agustin2051@gmail.com>
2023-09-30 18:16:59 -0300

    10

---
+++ src/app/components/Principal.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { Button } from "@nextui-org/react";
+const Principal = () => {
+  return (
+    <div className=" text-center text-white ">
+      <h1 className="font-bold text-5xl pt-10">We´re Jaguar</h1>
+      <p>
+        Focusing on the security, fidelity and ease of your money flow and
+        investments.
+      </p>
+      <Button className="bg-[#d44e86] text-white" variant="shadow">
+        Shadow
+      </Button>
+    </div>
+  );
+};
+
+export default Principal;
